10693d11382ebc5ad17e5f84c18272bf1f69d322
G Manish reddy <gmanishreddy07@gmail.com>
2025-07-24 13:10:31 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,18 +1,22 @@
 require('dotenv').config();
 const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
 const mongoose = require('mongoose');
-const Contact = require('./models/Contact');
-const app = express();
+const cors = require('cors');
 const path = require('path');
+const Contact = require('./models/Contact');
+
+const app = express();
+
 app.use(express.static(path.join(__dirname, '../public')));
 app.use(cors());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected..."))
-  .catch(err => console.error(err));
-app.post('/Submit', async (req, res) => {
+  .catch(err => console.error("MongoDB connection error:", err));
+
+app.post('/submit', async (req, res) => {
   const { name, email, phone, message } = req.body;
   if (!name || !email || !phone || !message) {
     return res.status(400).send('All fields are required');
@@ -23,9 +27,10 @@ app.post('/Submit', async (req, res) => {
     return res.status(201).send('Form submitted successfully');
   } catch (err) {
     console.error(err);
-    return res.status(500).send('Server error');
+    return res.status(500).send('Server error' +err.message);
   }
 });
+
 const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
