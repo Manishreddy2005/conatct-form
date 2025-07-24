25ed25d1329223aa0ef728e3c1aa624e4d2aba13
G Manish reddy <gmanishreddy07@gmail.com>
2025-07-24 12:59:02 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -26,7 +26,7 @@ app.post('/Submit', async (req, res) => {
     return res.status(500).send('Server error');
   }
 });
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
